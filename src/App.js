a5bcbec7d37518526c6969a91910da69959e2528
Bart Piasek <bartpiasek@icloud.com>
2020-12-28 14:57:03 +0100

    herosection, button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./components/Pages/HomePage/Home";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" />
+        <Route path="/" exact component={Home} />
       </Switch>
     </Router>
   );
---
+++ src/components/Pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Exclusive Access",
+  headline: "Unlimited",
+  description: "get acc",
+  buttonLabel: "get started",
+  imgStart: "",
+  img: "../../../../public/back2.png",
+  alt: "credit",
+};
