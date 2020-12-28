b41ae14a5578dfe81e876c06be2d2473e3253f1d
Bart Piasek <bartpiasek@icloud.com>
2020-12-28 16:57:40 +0100

    navbar css

--- src/components/Pages/HomePage/Data.js
+++ src/components/Pages/HomePage/Data.js
@@ -9,3 +9,39 @@ export const homeObjOne = {
   img: "images/back2.png",
   alt: "credit",
 };
+
+export const homeObjTwo = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  headline: "MY PROJECT",
+  description: "Python Django Javascript React",
+  buttonLabel: "My GitHub",
+  imgStart: "",
+  img: "images/back2.png",
+  alt: "credit",
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  headline: "HOW I LEARN?",
+  description: "Python Django Javascript React",
+  buttonLabel: "My GitHub",
+  imgStart: "",
+  img: "images/back2.png",
+  alt: "credit",
+};
+
+export const homeObjFour = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  headline: "CONTACT",
+  description: "Python Django Javascript React",
+  buttonLabel: "My GitHub",
+  imgStart: "",
+  img: "images/back2.png",
+  alt: "credit",
+};
--- src/components/Pages/HomePage/Home.js
+++ src/components/Pages/HomePage/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import HeroSection from "../../HeroSection/HeroSection";
-import { homeObjOne } from "./Data";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Data";
 
 function Home() {
   return (
     <div>
       <HeroSection {...homeObjOne} />
+      <HeroSection {...homeObjTwo} />
+      <HeroSection {...homeObjThree} />
+      <HeroSection {...homeObjFour} />
     </div>
   );
 }
