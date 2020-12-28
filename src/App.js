2cba6d473c6d422ea991362e662e16839152ef4b
Bart Piasek <bartpiasek@icloud.com>
2020-12-28 17:20:17 +0100

    footer design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./components/Pages/HomePage/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <small className="website-rights">BART_PIASEK © 2020</small>
+          <div className="social-icons">
+            <Link
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <FaFacebook />
+            </Link>
+            <Link
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Github"
+            >
+              <FaGithub />
+            </Link>
+            <Link
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
