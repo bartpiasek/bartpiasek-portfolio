015ca62911d39d9ed48d9ef5824856368ea5bb14
Bart Piasek <bartpiasek@icloud.com>
2021-01-01 01:05:04 +0100

    implement github acitivty component

---
+++ src/components/Pages/HowLearn/GithubActivity/GithubActivity.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import GitHubFeed from "react-github-activity";
+
+class GithubActivity extends Component {
+  render() {
+    const fullName = "Bart Piasek";
+    const userName = "bartpiasek";
+    const avatarUrl = "";
+    const events = [
+      /* data result from https://api.github.com/users/${bartpiasek}/events */
+    ];
+
+    return (
+      <GitHubFeed
+        fullName={fullName} // Provide Full Name as displayed on GitHub
+        userName={userName} // Provide User Name as displayed on Guthub
+        avatarUrl={avatarUrl} // Provide the avatar url of your github profile
+        events={events} // provide array of events using the users '/events' endpoint of github api
+      />
+    );
+  }
+}
+
+export default GithubActivity;
--- src/components/Pages/HowLearn/HowLearn.js
+++ src/components/Pages/HowLearn/HowLearn.js
@@ -2,12 +2,14 @@ import React from "react";
 import HeroSection from "../../HeroSection/HeroSection";
 import { HowLearn } from "./Data";
 import LearningLine from "./LearningLine/LearningLine";
+import GithubActivity from "./GithubActivity/GithubActivity";
 
 function HowILearn() {
   return (
     <div>
       <HeroSection {...HowLearn} />
       {/* <LearningLine /> */}
+      <GithubActivity />
     </div>
   );
 }
