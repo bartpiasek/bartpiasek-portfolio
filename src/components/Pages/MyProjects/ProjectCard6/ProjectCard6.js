933a6770b7a08b3293dc55dec103e327d3be1423
Bart Piasek <bartpiasek@icloud.com>
2021-01-01 13:47:33 +0100

    styling myprojects

--- src/components/Pages/MyProjects/ProjectCard6/ProjectCard6.js
+++ src/components/Pages/MyProjects/ProjectCard6/ProjectCard6.js
@@ -64,7 +64,10 @@ function ProjectCard() {
             </Link>
           </div>
         </div>
+      </div>
+      <div className="pricing__section">
         <div className="pricing__wrapper">
+          {/* <h1 className="pricing__heading">My Projects</h1> */}
           <div className="pricing__container">
             <Link to="/sign-up" className="pricing__container-card">
               <div className="pricing__container-cardInfo">
